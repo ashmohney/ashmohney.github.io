11a73721bed0cf339f53a750a1dfdf4fe615ff31
Ash Mohney <44374838+ashmohney@users.noreply.github.com>
2019-04-11 23:00:16 -0400

    UI updates and added projects

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ashmohney.github.io
 
 #####
-One page portfolio with an About Me, Contact,  and links to current projects. 
+One page portfolio with an About Me, Contact, and links to current projects. 
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,28 +1,2 @@
-$(document).ready(function(){
-
-    let gitClick = $("#icon1");
-    let linkClick = $("#icon2");
-    
-gitClick.on("click", function(gitOne){
-    window.open("https://github.com/ashmohney", '_blank');
-    console.log(gitOne);
-});
-// console.log(gitOne);
-
-linkClick.on("click", function(linkOne){
-    window.open("http://www.linkedin.com/in/ashmohney", '_blank');
-    console.log(linkOne);
-});
-
-
-
-{/* <a href=""></a> */}
-
-
-
-
-});
-
-
 
 
